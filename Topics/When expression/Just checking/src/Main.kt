7f8cec4df4e2d5d9815d187d96c4a68763247b79
Karina <99499702+kabrishka@users.noreply.github.com>
2023-01-16 23:16:37 +0300

    Delete Topics/When expression directory

--- Topics/When expression/Just checking/src/Main.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val str = readln()
-    val answer = when (str) {
-        "1","3","4" -> "No!"
-        "2" -> "Yes!"
-        else -> "Unknown number"
-    }
-    println(answer)
-}
